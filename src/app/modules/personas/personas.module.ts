28c38dc22bc16526837b0acaa38805fc5efd206a
Christia Roig <ch.roig@gmail.com>
2019-08-28 11:51:28 -0300

    Generaración de modulo Personas

---
+++ src/app/modules/personas/personas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonasComponent } from './personas.component';
+
+@NgModule({
+  declarations: [PersonasComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonasModule { }
